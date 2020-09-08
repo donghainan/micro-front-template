ce6fe2298018547e6c2d3882c5c093b9b90092c8
hainan dong <dhn610018574@sina.com>
2020-09-08 14:22:12 +0800

    init

---
+++ micro-app-main/src/routes/index.ts
@@ -0,0 +1,25 @@
+/*
+ * @Author: hainan dong
+ * @Date: 2020-09-08 11:22:47
+ * @LastEditTime: 2020-09-08 11:41:55
+ * @LastEditors: hainan dong
+ * @Description: 
+ * @FilePath: \ja-front-template\micro-app-main\src\routes\index.ts
+ * @Code Is Everything
+ */
+import Home from "@/views/home/index.vue";
+
+const routes = [
+  {
+    /**
+     * path: 路径为 / 时触发该路由规则
+     * name: 路由的 name 为 Home
+     * component: 触发路由时加载 `Home` 组件
+     */
+    path: "/",
+    name: "Home",
+    component: Home,
+  }
+];
+
+export default routes;
---
+++ micro-app-main/src/views/home/index.vue
@@ -0,0 +1,28 @@
+<!--
+ * @Author: hainan dong
+ * @Date: 2020-09-08 11:30:50
+ * @LastEditTime: 2020-09-08 11:30:54
+ * @LastEditors: hainan dong
+ * @Description: 
+ * @FilePath: \ja-front-template\micro-app-main\src\views\home\index.vue
+ * @Code Is Everything
+-->
+<template>
+<section class="home-container">Home</section>
+</template>
+
+<script lang="ts">
+import {
+  Component,
+  Vue
+} from "vue-property-decorator";
+
+@Component
+export default class Home extends Vue {}
+</script>
+
+<style lang="less" scoped>
+.home-container {
+  padding: 30px;
+}
+</style>
